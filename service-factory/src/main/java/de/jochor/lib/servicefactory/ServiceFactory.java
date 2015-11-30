4458ce4d96755a560c4d92c9857c15a8a49adb36
Jochen Hormes <jochen@jochor.de>
2015-11-30 22:21:14 +0100

    Changed visibility of ServiceFactory constructor.

--- service-factory/src/main/java/de/jochor/lib/servicefactory/ServiceFactory.java
+++ service-factory/src/main/java/de/jochor/lib/servicefactory/ServiceFactory.java
@@ -35,7 +35,7 @@ public abstract class ServiceFactory {
 		silence = Boolean.parseBoolean(silenceString);
 	}
 
-	private ServiceFactory() {
+	protected ServiceFactory() {
 		// Intended blank
 	}
 
